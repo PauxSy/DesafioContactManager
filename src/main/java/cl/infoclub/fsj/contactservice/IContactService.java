ac91fe4446ff8126dca3af4374d9395af404bed2
Paula Salgado <paula.salgado03@inacapmail.cl>
2022-11-15 19:56:42 -0300

    holi

---
+++ src/main/java/cl/infoclub/fsj/contactservice/IContactService.java
@@ -0,0 +1,5 @@
+package cl.infoclub.fsj.contactservice;
+
+public interface IContactService {
+
+}
---
+++ src/main/java/cl/infoclub/fsj/contactserviceimpl/ContactServiceImpl.java
@@ -0,0 +1,5 @@
+package cl.infoclub.fsj.contactserviceimpl;
+
+public class ContactServiceImpl {
+
+}
